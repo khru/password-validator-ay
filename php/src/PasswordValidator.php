16627af53d1f0d25aa3223c91f1a35c5c5876cc7
Emmanuel Valverde Ramos <emmanuel.valverde-ramos@aboutyou.com>
2024-03-24 11:57:18 +0100

    refactor: use abstraction for rule of lenght

--- php/src/PasswordValidator.php
+++ php/src/PasswordValidator.php
@@ -6,19 +6,10 @@ class PasswordValidator
 {
     public function theMethod(string $password): bool
     {
-        if ($password === 'P4sswd_') {
-            if (strlen($password) < 8) {
-                return false;
-            }
-        }
-
-        if ($password === 'pAssw0_') {
+        if (strlen($password) < 8) {
             return false;
         }
 
-        if ($password === 'Passw1_') {
-            return false;
-        }
         return true;
     }
 }
