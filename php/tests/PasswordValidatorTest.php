0053c63c219069eef5599fcbe005e17d1767481f
Emmanuel Valverde Ramos <emmanuel.valverde-ramos@aboutyou.com>
2024-03-24 11:46:35 +0100

    red: give_a_password_paSsw0__the_validator_should_fail

--- php/tests/PasswordValidatorTest.php
+++ php/tests/PasswordValidatorTest.php
@@ -30,4 +30,12 @@ class PasswordValidatorTest extends TestCase
 
         self::assertFalse($validator->theMethod('pAssw0_'));
     }
+
+    /** @test */
+    public function give_a_password_paSsw0__the_validator_should_fail(): void
+    {
+        $validator = new PasswordValidator();
+
+        self::assertFalse($validator->theMethod('paSsw0_'));
+    }
 }
