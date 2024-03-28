d6eac4b0fad81d6b6c939428dc01cb902a1471d1
Emmanuel Valverde Ramos <emmanuel.valverde-ramos@aboutyou.com>
2024-03-28 17:30:44 +0100

    refactor: change names

--- php/src/DefaultPasswordValidator.php
+++ php/src/DefaultPasswordValidator.php
@@ -11,41 +11,41 @@ class DefaultPasswordValidator
 
     public function theMethod(string $password): bool
     {
-        if ($this->hasMinimumLength($password)) {
+        if ($this->doesNotHaveMinimumLength($password)) {
             return false;
         }
 
-        if ($this->hasDigits($password)) {
+        if ($this->doesNotHaveDigits($password)) {
             return false;
         }
 
-        if ($this->hasUppercase($password)) {
+        if ($this->doesNotHaveUppercase($password)) {
             return false;
         }
 
-        if ($this->hasLowercase($password)) {
+        if ($this->doesNotHaveLowercase($password)) {
             return false;
         }
 
         return true;
     }
 
-    public function hasMinimumLength(string $password): bool
+    public function doesNotHaveMinimumLength(string $password): bool
     {
         return strlen($password) < self::MINIMUM_LENGTH;
     }
 
-    public function hasDigits(string $password): bool
+    public function doesNotHaveDigits(string $password): bool
     {
         return !preg_match(self::DIGITS_REGEX, $password);
     }
 
-    public function hasUppercase(string $password): bool
+    public function doesNotHaveUppercase(string $password): bool
     {
         return !preg_match(self::UPPER_REGEX, $password);
     }
 
-    public function hasLowercase(string $password): bool
+    public function doesNotHaveLowercase(string $password): bool
     {
         return !preg_match(self::LOWER_REGEX, $password);
     }
